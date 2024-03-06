54dc387cc5d37e9092978c3e0e701b26ccd3d6bd
KrNel <159314411+krisbelangernelson@users.noreply.github.com>
2024-03-06 18:13:34 -0500

    chore: husky

--- src/controllers/orders.ts
+++ src/controllers/orders.ts
@@ -2,20 +2,13 @@ import type { Request, Response } from 'express'
 import * as orders from '@/services/orders'
 import { errorResponses } from '@/utils/httpErrors/errorResponses'
 import { type Error } from '@/types/error'
+import { type CreateOrder } from '@/types/order'
 
 const createOrder = async (req: Request, res: Response): Promise<void> => {
   await orders
-    .createOrder(req.body)
+    .createOrder(req.body as CreateOrder)
     .then((results) => res.json(results))
     .catch((error: Error) => errorResponses(res, error, 'createOrder'))
 }
-
-// const getOrderByCustomerId = async (req: Request, res: Response): Promise<void> => {
-//   const { id } = req.params
-//   await orders
-//     .getOrderByCustomerId(id)
-//     .then((results) => res.json(results))
-//     .catch((error: Error) => errorResponses(res, error, 'getOrderByCustomerId'))
-// }
 
 export { createOrder }
--- src/services/orders.ts
+++ src/services/orders.ts
@@ -5,7 +5,3 @@ export const createOrder = async (body: CreateOrder): Promise<object> => {
   await insertOrder(body)
   return { code: '0' }
 }
-
-// export const getOrderByCustomerId = async (id: string): Promise<Order[]> => {
-//   return await findOrderByCustomerId(id)
-// }
